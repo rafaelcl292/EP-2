7bb94adf847f7ac3a462bcc10d81f6fb7308e72f
rafaelcl292 <rafaelcl@al.insper.edu.br>
2021-04-22 17:54:06 -0300

    .

--- AcademiaPython/cria_baralho.py
+++ AcademiaPython/cria_baralho.py
@@ -1,1 +1,9 @@
 # cria baralho
+def cria_baralho():
+    valores = [str(a) for a in range(2, 11)] + ['Q', 'J', 'K', 'A']
+    naipes = ['♠', '♥', '♣', '♦']
+    baralho = []
+    for valor in valores:
+        for naipe in naipes:
+            baralho.append(valor + naipe)
+    return baralho
