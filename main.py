1f38592885d0b694498819612a2ce5bf88e01b28
Maraba23 <78489243+Maraba23@users.noreply.github.com>
2021-04-23 17:11:48 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Paciência Acordeão") 
+print("==================")
+
+print("Seja bem-vindo(a) ao jogo de Paciência Acordeão! O objetivo deste jogo é colocar todas as cartas em uma mesma pilha.") 
+
+print("Existem apenas dois movimentos possíveis:") 
+
+print("1. Empilhar uma carta sobre a carta imediatamente anterior;") 
+print("2. Empilhar uma carta sobre a terceira carta anterior.") 
+
+print("Para que um movimento possa ser realizado basta que uma das duas condições abaixo seja atendida:") 
+
+print("1. As duas cartas possuem o mesmo valor ou") 
+print("2. As duas cartas possuem o mesmo naipe.") 
+
+print("Desde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada.") 
+
+input("Aperte [Enter] para iniciar o jogo...")
