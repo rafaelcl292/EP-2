7fae3b76ae69fb4d8de8d081dcad8c6a555b7ada
Maraba23 <78489243+Maraba23@users.noreply.github.com>
2021-04-23 16:42:54 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -28,3 +28,33 @@ def lista_movimentos_possiveis(baralho, i):
             if baralho[i][-1] == baralho[i-3][-1] or baralho[i][:-1] == baralho[i-3][:-1]:
                 return [3]
     return []
+
+def empilha(baralho, origem, destino):
+    carta_seleciona = baralho[origem]
+    baralho[destino] = carta_seleciona
+    del(baralho[origem])
+    return baralho
+    
+def lista_movimentos_possiveis(baralho, i):
+    if i != 0:
+        if i <= 2:
+            if baralho[i][-1] == baralho[i-1][-1] or baralho[i][:-1] == baralho[i-1][:-1]:
+                return [1]
+        else:
+            if (baralho[i][-1] == baralho[i-1][-1] or baralho[i][:-1] == baralho[i-1][:-1]) and (baralho[i][-1] == baralho[i-3][-1] or baralho[i][:-1] == baralho[i-3][:-1]):
+                return [1, 3]
+            if baralho[i][-1] == baralho[i-1][-1] or baralho[i][:-1] == baralho[i-1][:-1]:
+                return [1]
+            if baralho[i][-1] == baralho[i-3][-1] or baralho[i][:-1] == baralho[i-3][:-1]:
+                return [3]
+    return []
+
+
+def possui_movimentos_possiveis(baralho):
+    possui = False
+    i = 0
+    for _ in baralho:
+        if lista_movimentos_possiveis(baralho, i) != []:
+            possui = True
+        i += 1
+    return possui
